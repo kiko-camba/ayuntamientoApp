0b8266962a8236a7e581978ddec96bfb415bbe13
Kiko Camba <kikocamba82@gmail.com>
2024-10-01 22:43:32 +0200

    Add frontend structure

---
+++ ayuntamientoApp.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
+const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGO_URI;
+
+const app = express();
+
+
+app.use(express.json());
+
+const connectDB = async () =>{
+    try{
+        await mongoose.connect(mongoUri);
+        console.log('Database connected');
+        
+    }catch(error){
+        console.log(error);
+
+    }
+}
+connectDB()
+
+app.listen(port, () =>{
+    console.log(`Server running on port ${port}`)
+})
